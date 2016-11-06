7f574a26aefe83e66ecf96cd6583d26789fde27b
谢思聪 <bsknight58@gmail.com>
2016-11-06 23:13:05 +0800

    commit

--- train_bet_model.py
+++ train_bet_model.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+from time import time
 #import gensim
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cross_validation import train_test_split
@@ -31,6 +32,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import learning_curve
 from sklearn.model_selection import ShuffleSplit
+from matplotlib import offsetbox
+from sklearn import (manifold, datasets, decomposition, ensemble,
+                     discriminant_analysis, random_projection)
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -43,6 +47,150 @@ op.add_option("-u","--update",action="store_true", dest="update",help="update th
 
 comp_array = ["威廉希尔","澳门","立博","Bet365","Interwetten","SNAI","伟德","Bwin","Coral","SportingBet(博天堂)"]
 
+# Scale and visualize the embedding vectors
+def plot_embedding(X, y, title=None):
+    x_min, x_max = np.min(X, 0), np.max(X, 0)
+    X = (X - x_min) / (x_max - x_min)
+
+    plt.figure()
+    ax = plt.subplot(111)
+    for i in range(X.shape[0]):
+        plt.text(X[i, 0], X[i, 1], str(y[i]),
+                 color=plt.cm.Set1(y[i] / 10.),
+                 fontdict={'weight': 'bold', 'size': 9})
+    '''
+    digits = datasets.load_digits(n_class=2)
+    if hasattr(offsetbox, 'AnnotationBbox'):
+        # only print thumbnails with matplotlib > 1.0
+        shown_images = np.array([[1., 1.]])  # just something big
+        for i in range(X.shape[0]):
+            dist = np.sum((X[i] - shown_images) ** 2, 1)
+            if np.min(dist) < 4e-3:
+                # don't show points that are too close
+                continue
+            shown_images = np.r_[shown_images, [X[i]]]
+            imagebox = offsetbox.AnnotationBbox(
+                offsetbox.OffsetImage(digits.images[i], cmap=plt.cm.gray_r),
+                X[i])
+            ax.add_artist(imagebox)
+    '''
+    plt.xticks([]), plt.yticks([])
+    if title is not None:
+        plt.title(title)
+
+def dimension_reduce(X_train,y_train):
+    '''
+    rp = random_projection.SparseRandomProjection(n_components=2, random_state=42)
+    X_projected = rp.fit_transform(X_train)
+    plot_embedding(X_projected, y_train, "Random Projection of the digits")
+    
+    # Projection on to the first 2 principal components
+    print("Computing PCA projection")
+    t0 = time()
+    X_pca = decomposition.TruncatedSVD(n_components=2).fit_transform(X_train)
+    plot_embedding(X_pca, y_train
+                   "Principal Components projection of the digits (time %.2fs)" %
+                                  (time() - t0))
+    print("Computing Linear Discriminant Analysis projection")
+    X2 = X_train.copy()
+    X2.flat[::X_train.shape[1] + 1] += 0.01  # Make X invertible
+    t0 = time()
+    X_lda = discriminant_analysis.LinearDiscriminantAnalysis(n_components=2).fit_transform(X2, y_train)
+    plot_embedding(X_lda, y_train,
+                   "Linear Discriminant projection of the digits (time %.2fs)" %
+                                  (time() - t0))
+    '''
+    n_neighbors = 30
+    # Isomap projection of the digits dataset
+    print("Computing Isomap embedding")
+    t0 = time()
+    X_iso = manifold.Isomap(n_neighbors, n_components=2).fit_transform(X_train)
+    print("Done.")
+    plot_embedding(X_iso,y_train,
+                   "Isomap projection of the digits (time %.2fs)" %(time() - t0))
+    # Locally linear embedding of the digits dataset
+    print("Computing LLE embedding")
+    clf = manifold.LocallyLinearEmbedding(n_neighbors, n_components=2,
+                                          method='standard')
+    t0 = time()
+    X_lle = clf.fit_transform(X_train)
+    print("Done. Reconstruction error: %g" % clf.reconstruction_error_)
+    plot_embedding(X_lle,y_train,
+                   "Locally Linear Embedding of the digits (time %.2fs)" %
+                   (time() - t0))
+    # Modified Locally linear embedding of the digits dataset
+    print("Computing modified LLE embedding")
+    clf = manifold.LocallyLinearEmbedding(n_neighbors, n_components=2,
+                                          method='modified')
+    t0 = time()
+    X_mlle = clf.fit_transform(X_train)
+    print("Done. Reconstruction error: %g" % clf.reconstruction_error_)
+    plot_embedding(X_mlle,y_train,
+                   "Modified Locally Linear Embedding of the digits (time %.2fs)" %
+                   (time() - t0))
+    '''
+    # HLLE embedding of the digits dataset
+    print("Computing Hessian LLE embedding")
+    clf = manifold.LocallyLinearEmbedding(n_neighbors, n_components=2,
+                                          method='hessian')
+    t0 = time()
+    X_hlle = clf.fit_transform(X_train)
+    print("Done. Reconstruction error: %g" % clf.reconstruction_error_)
+    plot_embedding(X_hlle,y_train,
+                   "Hessian Locally Linear Embedding of the digits (time %.2fs)" %
+                   (time() - t0))
+    '''
+    # LTSA embedding of the digits dataset
+    print("Computing LTSA embedding")
+    clf = manifold.LocallyLinearEmbedding(n_neighbors, n_components=2,
+                                          method='ltsa')
+    t0 = time()
+    X_ltsa = clf.fit_transform(X_train)
+    print("Done. Reconstruction error: %g" % clf.reconstruction_error_)
+    plot_embedding(X_ltsa,y_train,
+                   "Local Tangent Space Alignment of the digits (time %.2fs)" %
+                   (time() - t0))
+    # MDS  embedding of the digits dataset
+    print("Computing MDS embedding")
+    clf = manifold.MDS(n_components=2, n_init=1, max_iter=100)
+    t0 = time()
+    X_mds = clf.fit_transform(X_train)
+    print("Done. Stress: %f" % clf.stress_)
+    plot_embedding(X_mds,y_train,
+                   "MDS embedding of the digits (time %.2fs)" %
+                   (time() - t0))
+    # Random Trees embedding of the digits dataset
+    print("Computing Totally Random Trees embedding")
+    hasher = ensemble.RandomTreesEmbedding(n_estimators=200, random_state=0,
+                                           max_depth=5)
+    t0 = time()
+    X_transformed = hasher.fit_transform(X_train)
+    pca = decomposition.TruncatedSVD(n_components=2)
+    X_reduced = pca.fit_transform(X_transformed)
+
+    plot_embedding(X_reduced,y_train,
+                   "Random forest embedding of the digits (time %.2fs)" %
+                   (time() - t0))
+    # Spectral embedding of the digits dataset
+    print("Computing Spectral embedding")
+    embedder = manifold.SpectralEmbedding(n_components=2, random_state=0,
+                                          eigen_solver="arpack")
+    t0 = time()
+    X_se = embedder.fit_transform(X_train)
+
+    plot_embedding(X_se,y_train,
+                   "Spectral embedding of the digits (time %.2fs)" %
+                   (time() - t0))
+    # t-SNE embedding of the digits dataset
+    print("Computing t-SNE embedding")
+    tsne = manifold.TSNE(n_components=2, init='pca', random_state=0)
+    t0 = time()
+    X_tsne = tsne.fit_transform(X_train)
+
+    plot_embedding(X_tsne,y_train,
+                   "t-SNE embedding of the digits (time %.2fs)" %
+                   (time() - t0))
+    plt.show()
 def plot_learning_curve(estimator, title, X, y, ylim=None, cv=None,
                         n_jobs=1, train_sizes=np.linspace(.1, 1.0, 5)):
     """
@@ -183,7 +331,7 @@ def process(corpus, target):
     plt.figure(figsize=(20, 20))
     _ = corrplot(df, annot=False)
     sns.plt.show()
-	'''
+    '''
     
     rint = random.randint(0,99);
     print "train_test_split randint %d" %(rint)
@@ -219,15 +367,12 @@ def process(corpus, target):
     print X_cv.shape
     print X_test.shape
     
-    title = "Learning Curves (SVM, RBF kernel, $\gamma=0.001$)"
-    cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
-    estimator = svm.SVC(gamma=0.001)
-    img = plot_learning_curve(estimator, title, X_train, y_train, cv=cv, n_jobs=4)
-    img.show()
-    estimator = MultinomialNB()
-    img = plot_learning_curve(estimator, title, X_train, y_train, cv=cv, n_jobs=4)
-    img.show()
-
+    #dimension_reduce(X_train, y_train)
+    tsne = manifold.TSNE(n_components=2, init='pca', random_state=0)
+    t0 = time()
+    X_tsne = tsne.fit_transform(X_train)
+    X_tsne_test = tsne.fit_transform(X_test)
+    
     score_type = 'precision'
     print "CV score:",score_type
     
@@ -261,9 +406,9 @@ def process(corpus, target):
         f.close()
         sys.exit()
     '''
-    #clf = svm.SVC(kernel = 'linear')
+    #clf = svm.SVC(kernel = 'rbf')
     parameters = {'C':[2**(-5),2**(-4),2**(-3),2**(-2),2**(-1),1,2**2],'kernel':['rbf','linear']}
-    clf = grid_search.GridSearchCV(svm.SVC(), parameters,cv=10)
+    clf = grid_search.GridSearchCV(svm.SVC(), parameters,cv=2)
     clf.fit(X_train,y_train)
     print clf.best_params_ 
     print clf.grid_scores_  
